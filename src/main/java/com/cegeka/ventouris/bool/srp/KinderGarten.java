77b69659aea5aab862fee5d27981c3114700b5f0
Roel Rymenants <roelrymenants@users.noreply.github.com>
2019-05-02 15:26:41 +0200

    Added KinderGarten + tests

--- src/main/java/com/cegeka/ventouris/bool/srp/KinderGarten.java
+++ src/main/java/com/cegeka/ventouris/bool/srp/KinderGarten.java
@@ -1,4 +1,56 @@
 package com.cegeka.ventouris.bool.srp;
 
+import java.util.Objects;
+
 public class KinderGarten {
+    private final int numberOfChildren;
+    private int numberOfCaregivers;
+
+    public KinderGarten(int numberOfChildren, int numberOfCaregivers) {
+        this.numberOfChildren = numberOfChildren;
+        this.numberOfCaregivers = numberOfCaregivers;
+    }
+
+    public String reportToParents() {
+        return Integer.toString(numberOfChildren);
+    }
+
+    public String reportToOwner() {
+        return String.join(":", reportToParents(), Integer.toString(numberOfCaregivers));
+    }
+
+    public String reportToAuditor() {
+        return Boolean.toString(numberOfChildren > 0 && numberOfChildren / (double) numberOfCaregivers > 6);
+    }
+
+    public static KinderGarten load(String serialized) {
+        String[] split = serialized.split(":");
+        int nChildren = Integer.parseInt(split[0]);
+        int nCaregivers = Integer.parseInt(split[1]);
+
+        return new KinderGarten(nChildren, nCaregivers);
+    }
+
+    public String save() {
+        return reportToOwner();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        KinderGarten that = (KinderGarten) o;
+        return numberOfChildren == that.numberOfChildren &&
+                numberOfCaregivers == that.numberOfCaregivers;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numberOfChildren, numberOfCaregivers);
+    }
+
+    @Override
+    public String toString() {
+        return KinderGarten.class.getCanonicalName() + "[" + reportToOwner() + "]";
+    }
 }
--- src/test/java/com/cegeka/ventouris/bool/srp/KinderGartenTest.java
+++ src/test/java/com/cegeka/ventouris/bool/srp/KinderGartenTest.java
@@ -1,4 +1,50 @@
 package com.cegeka.ventouris.bool.srp;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class KinderGartenTest {
+
+    @ParameterizedTest
+    @CsvSource({"0,0", "10,10"})
+    void kinderGarten_reportsNumberOfKidsToParents(int numberOfChildren, String expectedReport) {
+        KinderGarten kinderGarten = new KinderGarten(numberOfChildren, 0);
+
+        String actualReport = kinderGarten.reportToParents();
+
+        assertEquals(expectedReport, actualReport);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0,0:0", "10,2,10:2", "10,10,10:10"})
+    void kinderGarten_reportsNumberOfKidsAndCaregiversToOwner(int numberOfChildren, int numberOfCaregivers, String expectedReport) {
+        KinderGarten kinderGarten = new KinderGarten(numberOfChildren, numberOfCaregivers);
+
+        String actualReport = kinderGarten.reportToOwner();
+
+        assertEquals(expectedReport, actualReport);
+    }
+
+    @ParameterizedTest()
+    @CsvSource({"0,0,false", "1,1,false", "6,1,false", "7,1,true", "7,2,false", "600,99,true", "600,100,false", "607,100,true"})
+    void kinderGarten_reportToAuditorIfNotEnoughCaregivers(int numberOfChildren, int numberOfCaregivers, String expectedReport) {
+        KinderGarten kinderGarten = new KinderGarten(numberOfChildren, numberOfCaregivers);
+
+        String actualReport = kinderGarten.reportToAuditor();
+
+        assertEquals(expectedReport, actualReport);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,0", "10,10"})
+    void kinderGarten_canSaveAndLoad(int numberOfChildren, int numberOfCaregivers) {
+        KinderGarten kinderGarten = new KinderGarten(numberOfChildren, numberOfCaregivers);
+
+        String serialized = kinderGarten.save();
+        KinderGarten actualLoaded = KinderGarten.load(serialized);
+
+        assertEquals(kinderGarten, actualLoaded);
+    }
 }
